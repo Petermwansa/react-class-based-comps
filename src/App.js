a27c2fe796f22e184b092f5adbf27506d894b2a1
Petermwansa <mwansapeter800@gmail.com>
2024-06-16 13:10:46 +0300

    Exploring the use of the context with class based components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import Users from './components/Users';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Peter' },
+  { id: 'u2', name: 'Moses' },
+  { id: 'u3', name: 'Francis' },
+  { id: 'u4', name: 'Delphister' },
+  { id: 'u5', name: 'Agness' },
+  { id: 'u6', name: 'Brian' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
